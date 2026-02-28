d55c1aed56e87512c44832fdcf5f08834be2187d
Igor Sabadini <igorsabadini157@gmail.com>
2026-02-28 00:17:05 -0300

    feat: learning conditonals

---
+++ learning_conditionals/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+        int idade = 18;
+
+        if (idade >= 18) {
+            System.out.println("É [MAIOR] de idade !");
+        } else {
+            System.out.println("É [MENOR] de idade !");
+        }
+    }
+}
