67fd7f71b981e1cc723a547de070cede6badef3a
Igor Sabadini <igorsabadini157@gmail.com>
2026-02-27 23:58:29 -0300

    feat: learning class

---
+++ learning_class/src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    int ano;
+    String cor;
+    String marca;
+    String modelo;
+
+    void acelerar() {
+        System.out.println("O " + modelo + " da marca " + marca + " está acelerando...");
+        System.out.println("A cor do carro é " + cor + " e do ano " + ano);
+    }
+}
---
+++ learning_class/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main (String[] args) {
+        Carro meuCarro = new Carro();
+
+        meuCarro.modelo = "Hilux";
+        meuCarro.marca = "Toyota";
+        meuCarro.ano = 2025;
+        meuCarro.cor = "Branca";
+
+        Casa minhaCasa = new Casa();
+
+        minhaCasa.endereco = "Rua dos Alegres, 324, Santa Cruz";
+        minhaCasa.cor = "Vermelho";
+
+        System.out.println(minhaCasa.endereco);
+    }
+}
