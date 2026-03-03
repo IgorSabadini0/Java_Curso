83744caa62bbf85a9ade56005e06e0021d0f0058
Safadini <igorsabadini157@gmail.com>
2026-03-03 16:12:18 -0300

    feat: learning for

---
+++ learning_for/src/Contagem.java
@@ -0,0 +1,8 @@
+public class Contagem {
+    public void contar(int inicio, int fim, int passo) {
+        int i;
+        for (i = inicio; i <= fim; i = i + passo) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ learning_for/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        Contagem minhaContagem = new Contagem();
+
+        minhaContagem.contar(3,30,3);
+    }
+}
